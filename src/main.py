d4434de0bd9a9e11ff0f267dedc0236e4b91565f
4rthurC4stro <gomesarthurcastro@gmail.com>
2025-07-03 20:18:42 +0100

    adição de um timer em src/features/timer , mudei alguns caracteres que bugavam no windows mas ainda há oq mudar

--- src/main.py
+++ src/main.py
@@ -1,5 +1,6 @@
 from PyQt6.QtWidgets import QApplication
 from appmain.features.skill_master.skills_master_widget import SkillsMaster
+from appmain.database.database import db_create
 import sys
 
 
